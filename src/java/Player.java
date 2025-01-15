5146c3036a033edde74083aa840528b6c58ed8f4
steam38 <joel.chamberlain38@gmail.com>
2025-01-14 17:51:35 -0800

    Adding more logic for player

--- src/java/Player.java
+++ src/java/Player.java
@@ -1,3 +1,15 @@
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Player {
-    
+    private final String name;
+    private final Queue<Card> cards;
+
+    public Player(String name, List<Card> cards){
+        this.name = name;
+        this.cards = new LinkedList<>(cards);
+    }
+
 }
